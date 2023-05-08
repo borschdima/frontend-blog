29e81754e411a1d831220693b199f5c3faa3e260
Borshch Dmytro <borsch.dima@gmail.com>
2023-05-08 17:45:45 +0300

    Applied FSD structure

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,6 +1,6 @@
 import { FC, useMemo, useState } from 'react';
 
-import { LOCALE_STORATE_THEME_KEY, Theme, ThemeContext } from './ThemeContext';
+import { LOCALE_STORATE_THEME_KEY, Theme, ThemeContext } from '../lib/ThemeContext';
 
 const defaultTheme = (localStorage.getItem(LOCALE_STORATE_THEME_KEY) as Theme) || Theme.LIGHT;
 
